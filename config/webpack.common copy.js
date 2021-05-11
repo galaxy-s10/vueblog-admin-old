b9d37af9076d5c08f24b0766716e034f4d6105ca
galaxy-s10 <2274751790@qq.com>
2021-05-12 00:25:14 +0800

    0212

--- config/webpack.common copy.js
+++ config/webpack.common copy.js
@@ -30,13 +30,7 @@ const commonConfig = {
   },
   output: {
     filename: '[name]-bundle.js', //入口文件打包生成后的文件的文件名
-    /**
-     * 入口文件中，符合条件的代码，被抽离出来后生成的文件的文件名
-     * 如：动态(即异步)导入，默认不管大小，是一定会被单独抽离出来的。
-     * 如果一个模块既被同步引了，又被异步引入了，不管顺序（即不管是先同步引入再异步引入，还是先异步引入在同步引入），
-     * 这个模块会打包进bundle.js，而不会单独抽离出来。
-     */
-    chunkFilename: "[name]-[hash:6]-bundle-chunk.js", 
+    // chunkFilename: "[name]-[hash:6]-bundle-chunk.js", //入口文件中，符合条件的代码，被抽离出来后生成的文件的文件名
     path: path.resolve(__dirname, '../dist'),
     assetModuleFilename: "assets/[name]-[hash:6].[ext]", //静态资源生成目录（不管什么资源默认都统一生成到这里,除非单独设置了generator）
     // publicPath: "/abc" //output的publicPath建议与devServer的publicPath一致
@@ -60,12 +54,11 @@ const commonConfig = {
      * 比如：splitChunks.chunks:'async'等等，即会将异步代码抽离！
      */
     splitChunks: {  //对入口文件进行代码分离
-      // chunks: 'all',  //async,initial,all
-      // 如果minSize和maxSize冲突（即非法，如minSize:200,maxSize:100）控制台会报错。优先级：maxSize < minSize
+      chunks: 'all',  //async,initial,all
       minSize: 10 * 1024, //生成 chunk 的最小体积。默认：20000。
-      // maxSize: 10 * 1024,  //尝试将大于maxSize的chunk分割成较小的部分chunks。如果设置了initial或者all，建议必须设置maxSize，否则同步代码不会抽离！
-      // minRemainingSize: 0, //???
-      minChunks: 1, //模块被不同entry引用的次数大于等于才能分割。
+      // maxSize: 10 * 1024,
+      // minRemainingSize: 0,
+      minChunks: 1,
       maxAsyncRequests: 30, //按需加载时的最大并行请求数。默认：30
       maxInitialRequests: 30, //按需加载时的最大并行请求数。默认：30
       /**
@@ -74,12 +67,16 @@ const commonConfig = {
        * 允许拆分的包在10kb-30kb之间！
        */
       enforceSizeThreshold: 1 * 1024,  //默认：50000byte
-      // filename: "[id]-splitChunks.js", //不建议全局设置filename
-      /**
-       * 缓存组可以继承和/或覆盖来自 splitChunks.* 的任何选项
-       * 即如果匹配到缓存缓存组里的某一个，如vendor，vendor里的设置会对splitChunks的设置进行继承或覆盖
-       * 即vendor里没有设置chunks，vendor就会继承splitChunks的chunks，vendor设置了filename，会覆盖splitChunks的filename
-       */
+      filename: "[id]-splitChunks.js",
+      // chunks: "all",  //同步和异步代码都进行代码分离
+      // minChunks: 1, //模块被不同entry引用的次数大于等于才能分割。
+      // // 如果minSize和maxSize冲突（即非法，如minSize:200,maxSize:100），优先级：maxSize < minSize
+      // minSize: 3 * 1024,  //如果设置了mode: 'production'，miniSize默认就是30000！生成 chunk 的最小体积
+      // // maxSize: 40000,  //尝试将大于maxSize的chunk分割成较小的部分chunks。如果设置了initial或者all，建议必须设置maxSize，否则同步代码不会抽离！
+      // filename: "[id]-splitChunks.js",
+      // 缓存组可以继承和/或覆盖来自 splitChunks.* 的任何选项
+      // 即如果匹配到缓存缓存组里的某一个，如vendor，vendor里的设置会对splitChunks的设置进行继承或覆盖
+      // 即vendor里没有设置chunks，vendor就会继承splitChunks的chunks，vendor设置了filename，会覆盖splitChunks的filename
       cacheGroups: {  //cacheGroups里的优先级默认比外面的高
         // defaultVendors:false,  //禁用默认webpack默认设置的defaultVendors缓存组
         // default:false, //禁用默认webpack默认设置的default缓存组
@@ -88,12 +85,14 @@ const commonConfig = {
           test: /[\\/]node_modules[\\/]/,
           filename: '[name]-defaultVendors.js',
           priority: -10,  //优先级，优先使用优先级高的缓存组
+          reuseExistingChunk: true,
         },
-        default: {  //这是webpack4的默认设置
+        default: {
           chunks: 'all',
           filename: '[name]-default.js',
           minChunks: 2,
           priority: -20,
+          reuseExistingChunk: true,
         },
         test: {
           chunks: 'all',
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,7 @@
 // es6导出
 console.log('引入了untils.js')
+var aaa = 123
+window.aaa = aaa
 export var name = 'tom'
 export var sum = function (x, y) {
     return x + y
